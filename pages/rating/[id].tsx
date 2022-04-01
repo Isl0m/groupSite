63c941952fbf0fd2c03eebcd8d1bf45307c2e0cb
isl0m <islomrustam09.gmail.com>
2022-04-01 21:34:26 +0500

    routes.3

--- pages/rating/[id].tsx
+++ pages/rating/[id].tsx
@@ -16,87 +16,89 @@ import { rows, cell } from '../../consts';
 const RatingItem: React.FC = () => {
   const router = useRouter();
   const currentRow = rows.find((row) => row.num === Number(router.query.id));
-  if (!currentRow.name) {
-    return 'Error';
-  }
+
   return (
     <>
       <Layout>
-        <BackgroundLetterAvatars nameProps={currentRow.name} />
-        <Paper elevation={2} sx={{ maxWidth: '60vw', margin: '2rem auto' }}>
-          <TableContainer>
-            <Table sx={{ minWidth: 750 }} aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  <TableCell>{currentRow.name}</TableCell>
-                  <TableCell>Оценки</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                  <TableCell>{cell[1]}</TableCell>
-                  <TableCell>{currentRow.subjects[0]}</TableCell>
-                </TableRow>
-                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                  <TableCell>{cell[2]}</TableCell>
-                  <TableCell>{currentRow.subjects[1]}</TableCell>
-                </TableRow>
-                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                  <TableCell>{cell[3]}</TableCell>
-                  <TableCell>{currentRow.subjects[2]}</TableCell>
-                </TableRow>
-                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                  <TableCell>{cell[4]}</TableCell>
-                  <TableCell>{currentRow.subjects[3]}</TableCell>
-                </TableRow>
-                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                  <TableCell>{cell[5]}</TableCell>
-                  <TableCell>{currentRow.subjects[4]}</TableCell>
-                </TableRow>
-                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                  <TableCell>{cell[6]}</TableCell>
-                  <TableCell>{currentRow.subjects[5]}</TableCell>
-                </TableRow>
-                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                  <TableCell>{cell[7]}</TableCell>
-                  <TableCell>{currentRow.subjects[6]}</TableCell>
-                </TableRow>
-                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                  <TableCell>{cell[8]}</TableCell>
-                  <TableCell>{currentRow.subjects[7]}</TableCell>
-                </TableRow>
-                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                  <TableCell>{cell[9]}</TableCell>
-                  <TableCell>{currentRow.subjects[8]}</TableCell>
-                </TableRow>
-                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                  <TableCell>{cell[10]}</TableCell>
-                  <TableCell>{currentRow.subjects[9]}</TableCell>
-                </TableRow>
-                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                  <TableCell>{cell[11]}</TableCell>
-                  <TableCell>{currentRow.subjects[10]}</TableCell>
-                </TableRow>
-                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                  <TableCell>{cell[12]}</TableCell>
-                  <TableCell>{currentRow.subjects[11]}</TableCell>
-                </TableRow>
-                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                  <TableCell>{cell[13]}</TableCell>
-                  <TableCell>{currentRow.subjects[12]}</TableCell>
-                </TableRow>
-                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                  <TableCell>{cell[14]}</TableCell>
-                  <TableCell>{currentRow.subjects[13]}</TableCell>
-                </TableRow>
-                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                  <TableCell>{cell[15]}</TableCell>
-                  <TableCell>{currentRow.subjects[14]}</TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Paper>
+        {currentRow.name && (
+          <>
+            <BackgroundLetterAvatars nameProps={currentRow.name} />
+            <Paper elevation={2} sx={{ maxWidth: '60vw', margin: '2rem auto' }}>
+              <TableContainer>
+                <Table sx={{ minWidth: 750 }} aria-label="simple table">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>{currentRow.name}</TableCell>
+                      <TableCell>Оценки</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                      <TableCell>{cell[1]}</TableCell>
+                      <TableCell>{currentRow.subjects[0]}</TableCell>
+                    </TableRow>
+                    <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                      <TableCell>{cell[2]}</TableCell>
+                      <TableCell>{currentRow.subjects[1]}</TableCell>
+                    </TableRow>
+                    <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                      <TableCell>{cell[3]}</TableCell>
+                      <TableCell>{currentRow.subjects[2]}</TableCell>
+                    </TableRow>
+                    <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                      <TableCell>{cell[4]}</TableCell>
+                      <TableCell>{currentRow.subjects[3]}</TableCell>
+                    </TableRow>
+                    <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                      <TableCell>{cell[5]}</TableCell>
+                      <TableCell>{currentRow.subjects[4]}</TableCell>
+                    </TableRow>
+                    <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                      <TableCell>{cell[6]}</TableCell>
+                      <TableCell>{currentRow.subjects[5]}</TableCell>
+                    </TableRow>
+                    <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                      <TableCell>{cell[7]}</TableCell>
+                      <TableCell>{currentRow.subjects[6]}</TableCell>
+                    </TableRow>
+                    <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                      <TableCell>{cell[8]}</TableCell>
+                      <TableCell>{currentRow.subjects[7]}</TableCell>
+                    </TableRow>
+                    <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                      <TableCell>{cell[9]}</TableCell>
+                      <TableCell>{currentRow.subjects[8]}</TableCell>
+                    </TableRow>
+                    <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                      <TableCell>{cell[10]}</TableCell>
+                      <TableCell>{currentRow.subjects[9]}</TableCell>
+                    </TableRow>
+                    <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                      <TableCell>{cell[11]}</TableCell>
+                      <TableCell>{currentRow.subjects[10]}</TableCell>
+                    </TableRow>
+                    <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                      <TableCell>{cell[12]}</TableCell>
+                      <TableCell>{currentRow.subjects[11]}</TableCell>
+                    </TableRow>
+                    <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                      <TableCell>{cell[13]}</TableCell>
+                      <TableCell>{currentRow.subjects[12]}</TableCell>
+                    </TableRow>
+                    <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                      <TableCell>{cell[14]}</TableCell>
+                      <TableCell>{currentRow.subjects[13]}</TableCell>
+                    </TableRow>
+                    <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                      <TableCell>{cell[15]}</TableCell>
+                      <TableCell>{currentRow.subjects[14]}</TableCell>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </Paper>
+          </>
+        )}
       </Layout>
     </>
   );
