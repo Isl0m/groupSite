276d1163cb314d8c8f8eb88fcdd7b18cdf63e5db
isl0m <islomrustam09.gmail.com>
2022-04-07 22:47:00 +0500

    fix type err

--- pages/rating/[id].tsx
+++ pages/rating/[id].tsx
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router';
 import {
+  Typography,
   Table,
   TableBody,
   TableCell,
@@ -7,89 +8,68 @@ import {
   TableHead,
   TableRow,
   Paper,
+  Button,
 } from '@mui/material';
+import { grey } from '@mui/material/colors';
 
 import BackgroundLetterAvatars from '../../app/components/Avatar';
 import Layout from '../../app/layouts/Layout';
-import { rows, cell } from '../../consts';
+import { rows, cell, ITableRows } from '../../consts';
+
+const bgColor = grey[300];
 
 const RatingItem: React.FC = () => {
   const router = useRouter();
-  const currentRow = rows.find((row) => row.num === Number(router.query.id));
+
+  const data = rows.find((row) => row.num === Number(router.query.id));
+  if (data) {
+    const currentRow: ITableRows = data;
+  }
+  const currentRow: ITableRows = rows[0];
+
+  const getClassName = (item: string | number) => {
+    if (typeof item === 'number') {
+      return `color--${item} `;
+    }
+    return 'string';
+  };
+  const getAverage = (arr: number[]) => {
+    return Math.round(arr.reduce((acc, curr) => acc + curr, 0) / arr.length);
+  };
+  const average = getAverage(currentRow.subjects);
 
   return (
     <>
-      <Layout>
+      <Layout title={currentRow.name}>
         <BackgroundLetterAvatars nameProps={currentRow.name} />
-        <Paper elevation={2} sx={{ maxWidth: '60vw', margin: '2rem auto' }}>
+        <Typography variant="h3" component="h3" align="center" sx={{ marginTop: '2rem' }}>
+          {currentRow.name}
+        </Typography>
+        <Paper elevation={3} sx={{ maxWidth: '60vw', margin: '2rem auto' }}>
           <TableContainer>
             <Table sx={{ minWidth: 750 }} aria-label="simple table">
               <TableHead>
-                <TableRow>
-                  <TableCell>{currentRow.name}</TableCell>
+                <TableRow sx={{ backgroundColor: bgColor }}>
+                  <TableCell>Предметы</TableCell>
                   <TableCell>Оценки</TableCell>
                 </TableRow>
               </TableHead>
               <TableBody>
-                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                  <TableCell>{cell[1]}</TableCell>
-                  <TableCell>{currentRow.subjects[0]}</TableCell>
-                </TableRow>
-                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                  <TableCell>{cell[2]}</TableCell>
-                  <TableCell>{currentRow.subjects[1]}</TableCell>
-                </TableRow>
-                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                  <TableCell>{cell[3]}</TableCell>
-                  <TableCell>{currentRow.subjects[2]}</TableCell>
-                </TableRow>
-                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                  <TableCell>{cell[4]}</TableCell>
-                  <TableCell>{currentRow.subjects[3]}</TableCell>
-                </TableRow>
-                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                  <TableCell>{cell[5]}</TableCell>
-                  <TableCell>{currentRow.subjects[4]}</TableCell>
-                </TableRow>
-                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                  <TableCell>{cell[6]}</TableCell>
-                  <TableCell>{currentRow.subjects[5]}</TableCell>
-                </TableRow>
-                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                  <TableCell>{cell[7]}</TableCell>
-                  <TableCell>{currentRow.subjects[6]}</TableCell>
-                </TableRow>
-                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                  <TableCell>{cell[8]}</TableCell>
-                  <TableCell>{currentRow.subjects[7]}</TableCell>
-                </TableRow>
-                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                  <TableCell>{cell[9]}</TableCell>
-                  <TableCell>{currentRow.subjects[8]}</TableCell>
-                </TableRow>
-                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                  <TableCell>{cell[10]}</TableCell>
-                  <TableCell>{currentRow.subjects[9]}</TableCell>
-                </TableRow>
-                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                  <TableCell>{cell[11]}</TableCell>
-                  <TableCell>{currentRow.subjects[10]}</TableCell>
-                </TableRow>
-                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                  <TableCell>{cell[12]}</TableCell>
-                  <TableCell>{currentRow.subjects[11]}</TableCell>
-                </TableRow>
-                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                  <TableCell>{cell[13]}</TableCell>
-                  <TableCell>{currentRow.subjects[12]}</TableCell>
-                </TableRow>
-                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                  <TableCell>{cell[14]}</TableCell>
-                  <TableCell>{currentRow.subjects[13]}</TableCell>
-                </TableRow>
-                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                  <TableCell>{cell[15]}</TableCell>
-                  <TableCell>{currentRow.subjects[14]}</TableCell>
+                {currentRow.subjects.map((subject, idx) => (
+                  <TableRow
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    key={`${idx}-${subject}`}>
+                    <TableCell>{cell[++idx]}</TableCell>
+                    <TableCell className={getClassName(subject)}>{subject}</TableCell>
+                  </TableRow>
+                ))}
+                <TableRow
+                  sx={{
+                    '&:last-child td, &:last-child th': { border: 0 },
+                    backgroundColor: bgColor,
+                  }}>
+                  <TableCell>Средняя оценка</TableCell>
+                  <TableCell className={getClassName(average)}>{average}</TableCell>
                 </TableRow>
               </TableBody>
             </Table>
