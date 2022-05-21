3f570ff4d1b2c4cbf63b187f8096e01a6d73f604
isl0m <islomrustam09@gmail.com>
2022-05-21 21:22:23 +0500

    fix deploy bug

--- pages/rating/[id].tsx
+++ pages/rating/[id].tsx
@@ -23,77 +23,78 @@ const RatingItem: NextPage = () => {
   const dataID: number = Number(router.query.id) - 1;
   if (dataID < 0 || dataID > rows.length) {
     return null;
-  }
-  const currentRow: ITableRows = rows[dataID];
+  } else {
+    const currentRow: ITableRows = rows[dataID];
 
-  const getClassName = (item: string | number) => {
-    if (typeof item === 'number') {
-      return `color--${item} `;
-    }
-    return 'string';
-  };
-  const getAverage = (arr: number[]) => {
-    return Math.round(arr.reduce((acc, curr) => acc + curr, 0) / arr.length);
-  };
-  const average = getAverage(currentRow.subjects);
+    const getClassName = (item: string | number) => {
+      if (typeof item === 'number') {
+        return `color--${item} `;
+      }
+      return 'string';
+    };
+    const getAverage = (arr: number[]) => {
+      return Math.round(arr.reduce((acc, curr) => acc + curr, 0) / arr.length);
+    };
+    const average = getAverage(currentRow.subjects);
 
-  return (
-    <>
-      <Layout title={currentRow.name}>
-        <BackgroundLetterAvatars nameProps={currentRow.name} />
-        <Typography
-          variant="h3"
-          component="h3"
-          align="center"
-          sx={{ marginTop: '2rem' }}
-        >
-          {currentRow.name}
-        </Typography>
-        <Paper
-          elevation={3}
-          sx={{
-            maxWidth: { sm: '95vw', md: '80vw', lg: '65vw' },
-            margin: '2rem auto',
-          }}
-        >
-          <TableContainer>
-            <Table sx={{ minWidth: 750 }} aria-label="simple table">
-              <TableHead>
-                <TableRow sx={{ backgroundColor: bgColor }}>
-                  <TableCell>Предметы</TableCell>
-                  <TableCell>Оценки</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {currentRow.subjects.map((subject, idx) => (
+    return (
+      <>
+        <Layout title={currentRow.name}>
+          <BackgroundLetterAvatars nameProps={currentRow.name} />
+          <Typography
+            variant="h3"
+            component="h3"
+            align="center"
+            sx={{ marginTop: '2rem' }}
+          >
+            {currentRow.name}
+          </Typography>
+          <Paper
+            elevation={3}
+            sx={{
+              maxWidth: { sm: '95vw', md: '80vw', lg: '65vw' },
+              margin: '2rem auto',
+            }}
+          >
+            <TableContainer>
+              <Table sx={{ minWidth: 750 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow sx={{ backgroundColor: bgColor }}>
+                    <TableCell>Предметы</TableCell>
+                    <TableCell>Оценки</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {currentRow.subjects.map((subject, idx) => (
+                    <TableRow
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                      key={`${idx}-${subject}`}
+                    >
+                      <TableCell>{cell[++idx]}</TableCell>
+                      <TableCell className={getClassName(subject)}>
+                        {subject}
+                      </TableCell>
+                    </TableRow>
+                  ))}
                   <TableRow
-                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                    key={`${idx}-${subject}`}
+                    sx={{
+                      '&:last-child td, &:last-child th': { border: 0 },
+                      backgroundColor: bgColor,
+                    }}
                   >
-                    <TableCell>{cell[++idx]}</TableCell>
-                    <TableCell className={getClassName(subject)}>
-                      {subject}
+                    <TableCell>Средняя оценка</TableCell>
+                    <TableCell className={getClassName(average)}>
+                      {average}
                     </TableCell>
                   </TableRow>
-                ))}
-                <TableRow
-                  sx={{
-                    '&:last-child td, &:last-child th': { border: 0 },
-                    backgroundColor: bgColor,
-                  }}
-                >
-                  <TableCell>Средняя оценка</TableCell>
-                  <TableCell className={getClassName(average)}>
-                    {average}
-                  </TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Paper>
-      </Layout>
-    </>
-  );
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Paper>
+        </Layout>
+      </>
+    );
+  }
 };
 
 export default RatingItem;
